f9d0d00720100410327e85fcc54f511b2b6ee45f
gogamaga <jextailor@gmail.com>
2018-05-31 12:08:39 +0300

    change all js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ let video_length = +lengthInput.value;
 const leftThumb = document.querySelector('.left-thumb')
 const rightThumb = document.querySelector('.right-thumb')
 
-
 trimInput.addEventListener('input', (e) => {
     const {
         target
@@ -28,70 +27,42 @@ lengthInput.addEventListener('input', (e) => {
     console.log(target.value)
 })
 
-function width_right_thumb_container() {
+const mouse_move_trim = (e) => {
     const rangeWrap = document.querySelector('.range-wrap');
     const rangeWrap_container = document.querySelector('.right_thumb_container');
     const rangeWrap_width = rangeWrap.offsetWidth;
-    const rangeWrap_width_in_percent = rangeWrap_width / 100;
-    const leftThumb_position = leftThumb.style.left;
-    const width_right_thumb_container = parseInt(leftThumb_position) ? parseInt(leftThumb_position) : 0;
-    rangeWrap_container.style.width = 100 - width_right_thumb_container + '%'
-}
-
-
-
-const mouse_move_trim = (e) => {
-    const rangeWrap = document.querySelector('.range-wrap');
-    const rangeWrap_width = rangeWrap.offsetWidth;
-    const rangeWrap_width_in_percent = rangeWrap_width / 100
     const position_mouse = e.clientX - rangeWrap.getBoundingClientRect().left;
-    const position_mouse_in_percent = position_mouse / 100;
-    const next_position_left_thumb = position_mouse / rangeWrap_width_in_percent
-    if (next_position_left_thumb >= 0 && next_position_left_thumb <= 100) {
-        leftThumb.style.left = next_position_left_thumb + "%";
-        const length_in_percent = +video_length / 100;
-        trimInput.value = Math.round(next_position_left_thumb * length_in_percent)
-        lengthInput.value = video_length - +trimInput.value
-        width_right_thumb_container()
+    if (position_mouse >= 0 && position_mouse <= rangeWrap_width) {
+        leftThumb.style.left = position_mouse + "px";
+        console.log(right_thumb_container_width - parseInt(rangeWrap_container.style.right) - position_mouse)
+        rangeWrap_container.style.width = rangeWrap_width - parseInt(rangeWrap_container.style.right) - position_mouse + 'px';
+        // const length_in_percent = +video_length / 100;
+        // trimInput.value = Math.round(next_position_left_thumb * length_in_percent)
+        // lengthInput.value = +video_length - +trimInput.value
     }
 }
 
 leftThumb.addEventListener('mousedown', (e) => {
+    right_thumb_container_width = parseInt(right_thumb_container.style.width)
     rangeWrap.addEventListener('mousemove', mouse_move_trim)
 })
 
-function width_right_thumb_container_AND_pos_right() {
-    const rangeWrap = document.querySelector('.range-wrap');
-    const rangeWrap_container = document.querySelector('.right_thumb_container');
-    const rangeWrap_width = rangeWrap.offsetWidth;
-    console.log(rangeWrap_width, 1)
-    const rangeWrap_width_in_percent = rangeWrap_width / 100;
-    const rightThumb_position = rightThumb.style.left;
-    const width_right_thumb_container = parseInt(rightThumb_position) ? parseInt(rightThumb_position) : 100;
-    rangeWrap_container.style.right = 100 - width_right_thumb_container + '%';
-    rangeWrap_container.style.width = parseInt(rightThumb_position) + '%';
-
-}
+const right_thumb_container = document.querySelector('.right_thumb_container');
+let right_thumb_container_width;
 
 const mouse_move_length = function (e) {
-    width_right_thumb_container_AND_pos_right()
+    const rangeWrap = document.querySelector('.range-wrap');
     const right_thumb_container = document.querySelector('.right_thumb_container');
-    const rangeWrap_width = right_thumb_container.offsetWidth;
-
-    console.log(rangeWrap_width, 2)
-    const rangeWrap_width_in_percent = rangeWrap_width / 100;
-    const position_mouse = e.clientX - right_thumb_container.getBoundingClientRect().left;
-    const position_mouse_in_percent = position_mouse / 100;
-    const next_position_left_thumb = position_mouse / rangeWrap_width_in_percent;
-    if (next_position_left_thumb >= 0 && next_position_left_thumb <= 100) {
-        rightThumb.style.left = next_position_left_thumb + "%";
-        const length_in_percent = +video_length / 100;
-        lengthInput.value = Math.round(next_position_left_thumb * length_in_percent)
-
+    const rangeWrap_width = rangeWrap.offsetWidth;
+    const mouse_position = e.clientX - rangeWrap.getBoundingClientRect().left;
+    if (mouse_position >= 0 && mouse_position <= rangeWrap_width) {
+        right_thumb_container.style.width = mouse_position - parseInt(leftThumb.style.left) + 'px';
+        right_thumb_container.style.right = rangeWrap_width - mouse_position + 'px'
     }
 }
 
 rightThumb.addEventListener('mousedown', (e) => {
+    right_thumb_container_width = parseInt(right_thumb_container.style.width)
     rangeWrap.addEventListener('mousemove', mouse_move_length)
 })
 
@@ -103,5 +74,6 @@ document.addEventListener('mouseup', (e) => {
     rangeWrap.removeEventListener('mousemove', mouse_move_length)
 })
 
-
-width_right_thumb_container()
+const rangeWrap1 = document.querySelector('.range-wrap');
+const right_thumb_container1 = document.querySelector('.right_thumb_container');
+right_thumb_container.style.width = rangeWrap1.offsetWidth + 'px';
